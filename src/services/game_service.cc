60e5589a603dbcb703150579b5702759a76831e0
Aidan Bailey <BLYAID001@myuct.ac.za>
2020-12-19 01:29:53 +0200

    spruced game service + docs

--- src/services/game_service.cc
+++ src/services/game_service.cc
@@ -11,36 +11,51 @@
 #include <string>
 #include <vector>
 
+/**
+ * Flips colour to next player.
+ **/
+void chesspp::GameService::flipColour(void) {
+  to_play = to_play == Black ? White : Black;
+}
+
+/**
+ * Game service constructor.
+ **/
 chesspp::GameService::GameService(void)
     : to_play(chesspp::White), turn_counter(1), move_service(to_play, board),
       possible_move_list(move_service.GetAvailableTurns()), state(Playing){};
 
-chesspp::ChessColour chesspp::GameService::GetToPlay(void) { return to_play; }
-
-void chesspp::GameService::PrintBoard(void) {
-  std::cout << board.toString() << std::endl;
-  std::string colour = to_play == chesspp::White ? "White" : "Black";
-  switch (state) {
-  case Playing:
-    std::cout << colour << " to play." << std::endl;
-    break;
-  default:
-    std::cout << "Game over!" << std::endl
-              << chesspp::GameStateNames[state] << '!' << std::endl;
-  }
+/**
+ * Getter for current turn colour.
+ *
+ * @return ChessColour of this turn's player.
+ **/
+const chesspp::ChessColour &chesspp::GameService::GetToPlay(void) {
+  return to_play;
 }
 
-chesspp::GameState chesspp::GameService::GetState(void) const { return state; }
+/**
+ * Getter for current game state.
+ *
+ * @return Current GameState.
+ **/
+const chesspp::GameState &chesspp::GameService::GetState(void) { return state; }
 
-void chesspp::GameService::Concede(void) {
-  state = GameState(to_play == White ? Black : White);
-}
-
-std::vector<chesspp::Turn> chesspp::GameService::GetPossibleTurns() {
+/**
+ * Gets current possible turn list.
+ *
+ * @return Vector of possible turns.
+ **/
+const std::vector<chesspp::Turn> &chesspp::GameService::GetPossibleTurns(void) {
   return possible_move_list;
 };
 
-std::vector<std::pair<char, int>>
+/**
+ * Gets current moveable pieces.
+ *
+ * @return Vector of moveable pair locations.
+ **/
+const std::vector<std::pair<char, int>>
 chesspp::GameService::GetMoveablePieces(void) {
   std::vector<std::pair<char, int>> result{
       {possible_move_list[0].From.first, possible_move_list[0].From.second}};
@@ -53,7 +68,13 @@ chesspp::GameService::GetMoveablePieces(void) {
   return result;
 }
 
-std::vector<std::pair<char, int>>
+/**
+ * Gets possible moves for a specific piece.
+ *
+ * @param location Location of piece to process.
+ * @return Vector of possible target locations for the piece.
+ **/
+const std::vector<std::pair<char, int>>
 chesspp::GameService::GetPossibleMoves(std::pair<char, int> location) {
   std::vector<std::pair<char, int>> result;
   for (auto iter = possible_move_list.begin(); iter < possible_move_list.end();
@@ -63,6 +84,12 @@ chesspp::GameService::GetPossibleMoves(std::pair<char, int> location) {
   return result;
 }
 
+/**
+ * Attempts to play a turn.
+ *
+ * @param turn Turn to play.
+ * @return Whether or not the turn was successfully played.
+ **/
 bool chesspp::GameService::PlayTurn(Turn turn) {
   if (search::linear_search(turn, &possible_move_list[0],
                             possible_move_list.size()) == -1)
@@ -83,13 +110,38 @@ bool chesspp::GameService::PlayTurn(Turn turn) {
   return true;
 }
 
-void chesspp::GameService::flipColour(void) {
-  to_play = to_play == Black ? White : Black;
+/**
+ * Concedes the game for the current player.
+ **/
+void chesspp::GameService::Concede(void) {
+  if (state != Playing)
+    return;
+  state = GameState(to_play == White ? Black : White);
 }
 
+/**
+ * Resets the game.
+ **/
 void chesspp::GameService::ResetGame(void) {
   board.Reset();
-  state = Playing;
-  turn_counter = 1;
+  to_play = White;
   possible_move_list = move_service.GetAvailableTurns();
+  turn_counter = 1;
+  state = Playing;
+}
+
+/**
+ * Prints the board and current state.
+ **/
+void chesspp::GameService::PrintBoard(void) {
+  std::cout << board.toString() << std::endl;
+  std::string colour = to_play == chesspp::White ? "White" : "Black";
+  switch (state) {
+  case Playing:
+    std::cout << colour << " to play." << std::endl;
+    break;
+  default:
+    std::cout << "Game over!" << std::endl
+              << chesspp::GameStateNames[state] << '!' << std::endl;
+  }
 }
--- src/services/game_service.h
+++ src/services/game_service.h
@@ -11,26 +11,45 @@
 namespace chesspp {
 class GameService {
 private:
-  ChessColour to_play;
-  int turn_counter;
-  std::vector<Turn> turn_list;
-  Board board;
+  // move service
   MoveService move_service;
+  // board object
+  Board board;
+  // current turn colour variable
+  ChessColour to_play;
+  // current turn number
+  int turn_counter;
+  // current game history
+  std::vector<Turn> turn_list;
+  // current turn possible move list
   std::vector<Turn> possible_move_list;
+  // current game state
   GameState state;
+  // flips colour to next player
   void flipColour(void);
 
 public:
+  // game service default constructor
   GameService(void);
-  ChessColour GetToPlay(void);
-  void PrintBoard(void);
-  GameState GetState(void) const;
-  std::vector<Turn> GetPossibleTurns(void);
-  std::vector<std::pair<char, int>> GetMoveablePieces(void);
-  std::vector<std::pair<char, int>> GetPossibleMoves(std::pair<char, int>);
-  void Concede(void);
+  // getter for current turn colour
+  const ChessColour &GetToPlay(void);
+  // getter for current game state
+  const GameState &GetState(void);
+  // gets current possible turn list
+  const std::vector<Turn> &GetPossibleTurns(void);
+  // gets current moveable pieces
+  const std::vector<std::pair<char, int>> GetMoveablePieces(void);
+  // gets possible moves for a specific piece
+  const std::vector<std::pair<char, int>>
+      GetPossibleMoves(std::pair<char, int>);
+  // attempt to play a turn
   bool PlayTurn(Turn);
+  // concedes the game for the current player
+  void Concede(void);
+  // resets the game
   void ResetGame(void);
+  // prints the board and current state
+  void PrintBoard(void);
 };
 } // namespace chesspp
 
